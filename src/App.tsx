29e4a050e15736eaa7d4ef019f2d75e3302ccaeb
Jordan Ayala <jor.ayalag@gmail.com>
2025-09-19 12:01:16 -0300

    shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,16 @@ function Home() {
       <nav className="p-4 bg-gray-100 mb-8">
         <div className="max-w-4xl mx-auto flex gap-4 justify-center">
           <Link
-            to="/music"
+            to="/"
             className="text-blue-500 hover:text-blue-600 underline font-medium"
           >
             Home
           </Link>
           <Link
-            to="/"
+            to="/music"
             className="text-blue-500 hover:text-blue-600 underline font-medium"
           >
-            Video Test
+            Music Test
           </Link>
         </div>
       </nav>
